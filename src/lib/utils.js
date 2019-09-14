f52656a70a43f683f3aa703f0bb0c76534ff256b
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2019-09-14 17:33:55 +0200

    Implemented helper methods

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,64 @@
-function hash(str) {
-  throw new Error('Not implemented');
+const http = require("http");
+const https = require("https");
+const crypto = require("crypto");
+const { minify } = require("html-minifier");
+
+function md5(data) {
+  return crypto
+    .createHash("md5")
+    .update(data, "binary")
+    .digest("hex");
 }
 
 function openUrl(url) {
-  throw new Error('Not implemented');
+  return new Promise((resolve, reject) => {
+    let client = http;
+
+    if (url.toString().indexOf("https") === 0) {
+      client = https;
+    }
+
+    client
+      .get(url, resp => {
+        let data = "";
+
+        // A chunk of data has been recieved.
+        resp.on("data", chunk => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          resolve(data.toString());
+        });
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
 }
 
-function minifyContent(str) {
-  throw new Error('Not implemented');
+function minifyContent(html) {
+  const output = minify(html, {
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    collapseBooleanAttributes: true,
+  });
+  return output;
+}
+
+function hash(str) {
+  const result = md5(str);
+  return result;
+}
+
+async function pipeline(url) {
+  const rawHtml = await openUrl(url);
+  const minifiedHtml = minifyContent(rawHtml);
+  const hashed = hash(minifiedHtml);
+  return hashed;
 }
 
 module.exports = {
-  hash,
-  openUrl,
-  minifyContent,
+  pipeline,
 };
